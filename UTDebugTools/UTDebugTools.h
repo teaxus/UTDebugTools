e4b319dc70dc4953dda0a3aeefb314ff7384361a
🐹 <@gmail.com>
2020-04-19 10:00:37 +0800

    Initial Commit

---
+++ UTDebugTools/UTDebugTools.h
@@ -0,0 +1,19 @@
+//
+//  UTDebugTools.h
+//  UTDebugTools
+//
+//  Created by teaxus on 2020/4/19.
+//  Copyright © 2020 utaidev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UTDebugTools.
+FOUNDATION_EXPORT double UTDebugToolsVersionNumber;
+
+//! Project version string for UTDebugTools.
+FOUNDATION_EXPORT const unsigned char UTDebugToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UTDebugTools/PublicHeader.h>
+
+
